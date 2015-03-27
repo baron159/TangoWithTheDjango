ac244c089efd09c14879ff27aeac988915914b3c
Scott Baron <baron159@live.com>
2015-03-27 00:13:35 -0400

    Lesson 6 Done

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     context_dict = {'boldmessage': "I am bold font from the context"}
     return render(request, 'rango/index.html', context_dict)
 
+
 def about(request):
-    return HttpResponse("This is at the About Page <br/><a href='/rango/'>Index</a>")
+    message = {'myMessage': "This is my Page"}
+    return render(request, 'rango/about.html', message)
